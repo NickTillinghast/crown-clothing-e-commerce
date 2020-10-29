0c0489016899017b45078a226f9ff8b211bd212e
Nick Tillinghast <nickt2@cox.net>
2020-10-28 23:52:29 -0600

    added sass and .env

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import HomePage from './homepage.component';
+
+
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <HomePage />
     </div>
   );
 }
