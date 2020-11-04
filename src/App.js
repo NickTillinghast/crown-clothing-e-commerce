12bf7c9a3240bdd7f84ddd0426896328eb1fa37c
Nick Tillinghast <nickt2@cox.net>
2020-11-03 21:11:55 -0700

    added header and styles

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS pages</h1>
-  </div>
-)
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS pages</h1>
+//   </div>
+// )
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
         <Route path='/shop' component={ShopPage} />
-        {/* <HomePage /> */}
       </Switch>
     </div>
   );
