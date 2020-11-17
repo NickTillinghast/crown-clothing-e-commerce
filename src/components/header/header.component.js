5adceea290cc466d1a23b3a3f8c43ad68c5a9527
Nick Tillinghast <nickt2@cox.net>
2020-11-16 23:23:42 -0700

    added redux and implemented userReducer and header actions

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,7 +1,13 @@
 import React from 'react';
+
+import { connect } from 'react-redux';
+
 import { Link } from 'react-router-dom';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import './header.styles.scss';
+
 import { auth } from '../../firebase/firebase.util';
 
 const Header = ({ currentUser }) => (
@@ -17,7 +23,9 @@ const Header = ({ currentUser }) => (
                 CONTACT
         </Link>
             {
-                currentUser ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                currentUser ? <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                    </div>
                     : <Link className='option' to='/signin'>
                         SIGN IN
              </Link>
@@ -26,4 +34,8 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
