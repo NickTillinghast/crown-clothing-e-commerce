6ac8871d0e6cb4a1af82f3ac2afbd18855a9c2c8
Nick Tillinghast <nickt2@cox.net>
2020-11-17 23:32:22 -0700

    added cart icon

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -8,6 +8,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { auth } from '../../firebase/firebase.util';
 
 const Header = ({ currentUser }) => (
@@ -32,6 +33,7 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
