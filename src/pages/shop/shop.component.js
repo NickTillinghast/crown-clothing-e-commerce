d477675a48466aa3c17af54136e7bd4d1e3d9a56
Nick Tillinghast <nickt2@cox.net>
2020-11-22 00:20:41 -0700

    put shop data in redux and created collection component

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import SHOP_DATA from './shop.data';
+// import { connect } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
+// import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+// import { selectCollections } from '../../redux/shop/shop.selectors';
+// import SHOP_DATA from './shop.data';
+import CollectionsOverview from '../..//components/collections-overview/collections-overview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const { collections } = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        <CollectionsOverview />
 
-        </div>)
-    }
-}
+    </div>
+)
+// const mapStateToProps = createStructuredSelector(
+//     { collections: selectCollections }
+// )
 
 export default ShopPage;
