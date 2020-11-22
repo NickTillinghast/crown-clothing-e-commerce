7379ef88e4fd9cba4d2a64baea233f73d39c9491
Nick Tillinghast <nickt2@cox.net>
2020-11-22 15:16:05 -0700

    added collection map and wired it to shop page and redux selectors

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,19 +1,17 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import { createStructuredSelector } from 'reselect';
-// import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-// import { selectCollections } from '../../redux/shop/shop.selectors';
-// import SHOP_DATA from './shop.data';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../..//components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => (
-    <div className='shop-page'>
-        <CollectionsOverview />
 
-    </div>
-)
-// const mapStateToProps = createStructuredSelector(
-//     { collections: selectCollections }
-// )
+const ShopPage = ({ match }) => {
+    console.log(match)
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        </div>
+    )
+}
 
 export default ShopPage;
